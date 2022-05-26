8c8973a1a09bacacc2bf58aa66c1554700407334
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-26 13:07:20 +0300

    add

---
+++ src/components/home/mainContent/popular/Popular.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from '../../../common/heading/Heading';
+
+const Popular = () => {
+    return (
+        <>
+            <section className='popular'>
+                <Heading title='hello' />
+            </section>
+        </>
+    );
+};
+
+export default Popular;
