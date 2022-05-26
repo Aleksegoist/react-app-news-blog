fa0525b02675a4b48ebc8a43c9273a617f126f10
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-26 17:52:04 +0300

    add

--- src/components/home/mainContent/Ppost/Ppost.jsx
+++ src/components/home/mainContent/Ppost/Ppost.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { ppost } from '../../../../dummyData';
+import Heading from '../../../common/heading/Heading';
+import Slider from 'react-slick';
+import './ppost.css';
 
 const Ppost = () => {
-    return <div>Ppost</div>;
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    };
+    return (
+        <>
+            <section className='popularPost'>
+                <Heading title='Popular Posts' />
+                <div className='content'>
+                    <Slider {...settings}>
+                        {ppost.map((val) => {
+                            return (
+                                <div className='items'>
+                                    <div className='box shadow'>
+                                        <div className='images'>
+                                            <div className='img'>
+                                                <img
+                                                    src={val.cover}
+                                                    alt='popular'
+                                                />
+                                            </div>
+                                            <div className='category category1'>
+                                                <span>{val.category}</span>
+                                            </div>
+                                        </div>
+                                        <div className='text'>
+                                            <h1 className='title'>
+                                                {val.title.slice(0, 40)}...
+                                            </h1>
+                                            <div className='date'>
+                                                <i className='fa fa-calendar'></i>
+                                                <label>{val.date}</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </Slider>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Ppost;
