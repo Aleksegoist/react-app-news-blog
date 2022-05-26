1c7e46cdb21029f05a8b167f0ab087d68708de8d
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-26 23:41:47 +0300

    add

--- src/components/home/sideContent/side/Side.jsx
+++ src/components/home/sideContent/side/Side.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Slider from 'react-slick/lib/slider';
+import { gallery } from '../../../../dummyData';
 import Heading from '../../../common/heading/Heading';
 import SocialMedia from '../social/SocialMedia';
+import Tpost from '../tpost/Tpost';
 import './side.css';
 
 const Side = () => {
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    const category = [
+        'world',
+        'travel',
+        'sport',
+        'fun',
+        'health',
+        'fashion',
+        'business',
+        'technology',
+    ];
     return (
         <>
             <Heading title='Stay Connected' />
@@ -20,6 +41,31 @@ const Side = () => {
             </section>
             <section className='banner'>
                 <img src='./images/news_banner.jpg' alt='banner' />
+            </section>
+            <Tpost />
+
+            <section className='catgeorys'>
+                <Heading title='Categories' />
+                {category.map((val) => {
+                    return (
+                        <div className='category category1'>
+                            <span>{val}</span>
+                        </div>
+                    );
+                })}
+            </section>
+
+            <section className='gallery'>
+                <Heading title='Gallery' />
+                <Slider {...settings}>
+                    {gallery.map((val) => {
+                        return (
+                            <div className='img'>
+                                <img src={val.cover} alt='' />
+                            </div>
+                        );
+                    })}
+                </Slider>
             </section>
         </>
     );
--- src/components/home/sideContent/tpost/Tpost.jsx
+++ src/components/home/sideContent/tpost/Tpost.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { tpost } from '../../../../dummyData';
+import Heading from '../../../common/heading/Heading';
+import './tpost.css';
 
 const Tpost = () => {
-    return <div>Tpost</div>;
+    return (
+        <>
+            <section className='tpost'>
+                <Heading title='Tiktok Post' />
+                {tpost.map((val) => {
+                    return (
+                        <div className='box flexSB'>
+                            <div className='img'>
+                                <img src={val.cover} alt='' />
+                            </div>
+                            <div className='text'>
+                                <h1 className='title'>
+                                    {val.title.slice(0, 35)}...
+                                </h1>
+                                <span> year ago</span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </section>
+        </>
+    );
 };
 
 export default Tpost;
